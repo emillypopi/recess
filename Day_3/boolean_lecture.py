e9d4c603c0c5bafcd34b8801ae7d85d1d9bd6172
emillypopi <emillyshirley6@gmail.com>
2026-06-12 09:46:35 +0300

    day 3 lecture exercise

---
+++ Day_3/boolean_lecture.py
@@ -0,0 +1,23 @@
+a=0
+b=2
+c=4
+
+if a > b and b > c:
+    print("true")
+else:
+    print("false")
+
+    if a > b or b > c:
+        print("true")
+    else:
+        print("false")
+
+        if a==b:
+            print("true")
+        else:
+            print("false")
+
+            if a != b:
+                print("true")   
+            else:
+                print("false")
