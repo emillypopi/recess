b413d4a965f4c473a38211354e8b96c5aebb767c
emillypopi <emillyshirley6@gmail.com>
2026-06-26 18:26:33 +0300

    m

--- Day_9/realwrld.py
+++ Day_9/realwrld.py
@@ -6,10 +6,23 @@
 #    file.write(name)
  #   file.write('\n')
 
+import os
 import csv
-with open('student.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
+
+script_dir = os.path.dirname(__file__)
+csv_path = os.path.join(script_dir, 'student.csv')
+
+with open(csv_path, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+        
 #add ['regno', 'name', 'age', 'gender', 'course']
 #to the students.csv, using a dictionary csv writer
+
+with open(csv_path, 'a', newline='') as file:
+    fieldnames = ['regno', 'name', 'age', 'gender', 'course']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writerow({'regno': '24/U/08843/PS', 'name': 'Naluyange Emilly Shirley', 'age': '20', 'gender': 'Female', 'course': 'Software Engineering'})
+    file.write('\n')
+    
